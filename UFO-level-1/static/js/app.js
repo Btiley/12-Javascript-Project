429d82ad711bc1884131d85604359e243f113957
Brandon Tiley <bctiley@gmail.com>
2021-03-18 12:01:18 +1100

    Uploading complete UFO level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,53 +1,49 @@
-// Get the Data
-var sightings = data;
+// Get the Data (const to prevent change).
+const sightings = data;
 //Select table, input box and filter button elements
 var tbody = d3.select("tbody");
 // Select input box and button
-var form = d3.select("#form");
-var button = d3.select("#filter-btn");
+  var form = d3.select("#form");
+  var button = d3.select("#filter-btn");
 
-//Create event handlers
-button.on("click",runEnter);
-form.on("submit",runEnter);
+//Function to generate complete table based on ufo data
+function tablecreate(data){
+  tbody.html("");
+  data.forEach((sightings) => {
+        
+    //Create new row for each item in filtered data
+   var row = tbody.append("tr");
+    
+    //Create and append cell for each data value
+    
+    Object.entries(sightings).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+    });
+});
 
-function runEnter(){
-  
-  d3.event.preventDefault();
 
-  //select the input element
-  var inputElement = d3.select("datetime");
-
-  //get the value of input element
-  var inputValue = inputElement.property("value");
-
-  console.log(inputValue);
 }
+//Function to obtain filtered data based on user input and 
+function tablefilter(){
+  console.log("intialized");
+    
+    d3.event.preventDefault();
 
+    var dateInput = d3.select("#datetime").property("value");
 
+    var completeTable = sightings;
 
+    if (dateInput){
+      completeTable = completeTable.filter(ufo => ufo.datetime === dateInput);
 
-//filterbutton.on("click",runFilter);
-//inputBox.on("submit",runFilter);
+    }
+    tablecreate(completeTable);
 
-//function runFilter(){
-  //  d3.event.preventDefault();
-    //var inputElement = d3.select("datetime");
-    //var inputValue = inputElement.property("value");
+   };
+tablecreate(sightings);
 
-    //console.log(inputValue);
-   // console.log(sightings);
-
-   // var filteredSightings = sightings.filter(ufo => ufo.date);
-   // console.log(filteredSightings);}
-
-
-
- //   data.forEach((filteredSightings) => {
-   //     var row = tbody.append("tr");
-     //   Object.entries(filteredSightings).forEach(([key, value]) => {
-       // var cell = row.append("td");
-       // cell.text(value);
-       // });
- // });
+button.on("click",tablefilter);
+form.on("submit",tablefilter);
 
 
